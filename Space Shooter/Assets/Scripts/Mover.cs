6d53ba196ca3d27cf643084b030f82cb336e549c
handerson5991 <handerson5991@gmail.com>
2019-03-11 22:10:22 -0500

    Added ship, lighting, background, bolt

---
+++ Space Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+	void start(){
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+	}
+}
